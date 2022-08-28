15aa54be04be3c7848887e444ff4b4ac15616e23
Vale-pixel <100896964@u.icesi.edu.co>
2022-08-28 18:31:47 -0500

    Set up Websocket server.

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 const express = require("express");
+//nuestro sevidor
 const { Server } = require("socket.io");
+//ngrok y el puerto poner el mismo puerto
+const PORT = 5050; // No cambiar
+const SERVER_IP = "172.30.56.202"; // Cambiar por la IP del computador
 
-const expressApp = express(); //Environment setup
+//const os = require('os');
+//const IPaddress = os.networkInterfaces().en0[1].address;
 
-expressApp.use(express.json()); //Middlewares
-expressApp.use("/app", express.static("public")); //Middlewares
+const app = express();
+app.use(express.json());
+app.use("/app", express.static("public-app"));
+app.use("/mupi", express.static("public-mupi"));
 
-const httpServer = expressApp.listen(5050, () => {
-  //Star the server
-  console.log(`http://localhost:5050/app`);
+//crear servidor http para arrancar con el puerto
+const httpServer = app.listen(PORT, () => {
+  console.log(`http://${SERVER_IP}:${PORT}/app`);
+  console.log(`http://${SERVER_IP}:${PORT}/mupi`);
 });
+// Run on terminal: ngrok http 5050;
 
-const io = new Server(httpServer, { path: "/real-time" }); //WebSocket Server (instance) initialization
-//on = es igual a listen
+const io = new Server(httpServer, { path: "/real-time" });
+//aqupi ponemos los eventos que necesitamos 
 io.on("connection", (socket) => {
-  //Listening for webSocket connections
-  console.log("connected", socket.id);
-  //escuchar el evento
-  socket.on("positions", (message) => {
-    console.log(message);
-    //evitar que nos regresa la misma posición de nosotros - el eco
-    socket.broadcast.emit("display-positions", message);
+  console.log(socket.id);
+
+  socket.on("device-size", (deviceSize) => {
+    socket.broadcast.emit("mupi-size", deviceSize);
+  });
+
+  socket.on("mobile-instructions", (instructions) => {
+    console.log(instructions);
+    socket.broadcast.emit("mupi-instructions", instructions);
   });
 });
